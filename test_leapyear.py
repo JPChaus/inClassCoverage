90def9c184597180d8e47a23ca66b85f55f604ca
JPChaus <48576328+JPChaus@users.noreply.github.com>
2021-05-20 19:53:09 -0400

    ran test

--- test_leapyear.py
+++ test_leapyear.py
@@ -1,17 +1,18 @@
+import unittest
 import Leapyear
 
-def test_isLeapYear1():
-    result = Leapyear.isLeapYear(2000)
-    assert result == "2000 is a Leap Year."
+class test_Cases(unittest.TestCase):
+    def test_isLeapYear1(self):
+        self.assertEqual(Leapyear.isLeapYear(2000), "2000 is a Leap Year.")
 
-def test_isLeapYear2():
-    result = Leapyear.isLeapYear(1900)
-    assert result == "1900 is not a Leap Year."
+    def test_isLeapYear2(self):
+        self.assertEqual(Leapyear.isLeapYear(1900), "1900 is not a Leap Year.")
 
-def test_isLeapYear3():
-    result = Leapyear.isLeapYear(2001)
-    assert result == "2001 is not a Leap Year."
+    def test_isLeapYear3(self):
+        self.assertEqual(Leapyear.isLeapYear(2001), "2001 is not a Leap Year.")
 
-def test_isLeapYear4():
-    result = Leapyear.isLeapYear(2004)
-    assert result == "2004 is a Leap Year."
+    def test_isLeapYear4(self):
+        self.assertEqual(Leapyear.isLeapYear(2004), "2004 is a Leap Year.")
+
+if __name__ == '__main__':
+    unittest.main()

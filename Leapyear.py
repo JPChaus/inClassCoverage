87d2ed8c908447f8fd04e785bc92dedb5bd8a370
JPChaus <48576328+JPChaus@users.noreply.github.com>
2021-05-20 19:34:15 -0400

    updated code

--- Leapyear.py
+++ Leapyear.py
@@ -4,20 +4,27 @@
 #Program that asks for a year from the user and then determines if it is a leap year or not.
 #Repatedly asks the user for a new number if the number given is not a positive integer
 
-while True:
-    try:
-        year = int(input("Please enter a year. Positive integer values only: "))
-        if(year < 0):
-            raise ValueError
-        break
-    except ValueError:
-        print("Invalid input!")
+def main():
+    while True:
+        try:
+            year = int(input("Please enter a year. Positive integer values only: "))
+            if(year < 0):
+                raise ValueError
+            break
+        except ValueError:
+            print("Invalid input!")
+    
+    print(isLeapYear(year))
 
-if (year%400 == 0):
-    print("% s is a Leap Year." % year)
-elif (year%100 == 0):
-    print("% s is not a Leap Year." % year)
-elif (year%4 == 0):
-    print("% s is a Leap Year." % year)
-else:
-    print("% s is not a Leap Year." % year)
+def isLeapYear(year):
+    if (year%400 == 0):
+        return ("% s is a Leap Year." % year)
+    elif (year%100 == 0):
+        return ("% s is not a Leap Year." % year)
+    elif (year%4 == 0):
+        return ("% s is a Leap Year." % year)
+    else:
+        return ("% s is not a Leap Year." % year)
+
+if __name__ == '__main__':
+    main()
